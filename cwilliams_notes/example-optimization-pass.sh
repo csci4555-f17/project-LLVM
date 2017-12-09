e2ef0d4abc5af7f8dbf05a11957bd02db9cc2431
cwillia9 <coltonrwilliams@gmail.com>
2017-12-09 15:33:26 -0700

    cleanup

--- cwilliams_notes/example-optimization-pass.sh
+++ cwilliams_notes/example-optimization-pass.sh
@@ -1,3 +1,4 @@
+# Generate some dumb pyhton code
 cat >/tmp/stupidcode.py <<EOL
 a = 1+2
 a = 1+2
@@ -15,6 +16,13 @@ a = 1+2
 print a
 EOL
 
+# Compile python into llvm ir
 python /opt/pyyc-hellocompiler/hellocompiler/compile.py /tmp/stupidcode.py
-opt-5.0 -S -o /tmp/lessstupidir.ll -adce /tmp/stupidcode.ll 2> /tmp/lessstupidir.ll
+# Run an optimization pass on the IR and generate some hopefully better IR
+# -S prints output as IR
+# -o print output to specified file
+# -adce stands for 'aggresive dead code elimination'
+opt-5.0 -S -o /tmp/lessstupidir.ll -adce /tmp/stupidcode.ll
+
+# Run the new ir
 python /root/simplecompile.py /tmp/lessstupidir.ll
